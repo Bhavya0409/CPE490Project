56be0ab192a2b9ece15104201ce6db19962b9726
Bhavya Shah <Bhavya Shah>
2018-04-25 15:28:36 -0400

    init commit

---
+++ src/Client.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client
+{
+
+    public static void main(String args[]) throws IOException
+    {
+        Scanner scn = new Scanner(System.in);
+
+        // getting localhost ip
+        InetAddress ip = InetAddress.getByName("127.0.0.1");
+
+        // establish the connection
+        Socket s = new Socket(ip, 1234);
+
+        // obtaining input and out streams
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+        // sendMessage thread
+        Thread sendMessage = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+
+                    // read the message to deliver.
+                    String msg = scn.nextLine();
+
+                    try {
+                        // write on the output stream
+                        dos.writeUTF(msg);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        // readMessage thread
+        Thread readMessage = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (true) {
+                    try {
+                        // read the message sent to this client
+                        String msg = dis.readUTF();
+                        System.out.println(msg);
+                    } catch (IOException e) {
+
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        sendMessage.start();
+        readMessage.start();
+
+    }
+}
